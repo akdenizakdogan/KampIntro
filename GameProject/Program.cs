11b1f4aa67057ca20df2070632c19e5eb51fb565
Akdeniz Akdogan <Akdeniz Akdogan@AKDENIZ>
2022-03-01 20:44:05 +0300

    01.03 GamerProject

---
+++ GameProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new NewEStateUserValidationManager());
+            gamerManager.Add(new Gamer { Id = 1, BirthYear = 1996, FirstName = "Akdeniz", LastName = "Akdogan", IdentityNumber = 12345 });
+        }
+    }
+}
